7519e274dcfd4d87494ea0dff1b8ee5cbc055389
Antonio Gil <tritonmedias@gmail.com>
2018-06-16 13:43:29 -0430

    Usuario y token listos

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 
 //Inicializar variables
 var app = express();
 
+
+//Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+//Importar Rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 //Conexion a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
@@ -18,15 +29,9 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
 
 
 //Rutas
-app.get('/', (req, res, next) => {
-
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Petición realizada correctamente'
-    })
-})
-
-
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 //Escuchar peticiones
 app.listen(3000, () => {
---
+++ config/config.js
@@ -0,0 +1,2 @@
+//Constantes a manejar
+module.exports.SEED = 'este-es-un-seed-dificil';
